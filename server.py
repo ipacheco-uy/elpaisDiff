1ca761bd81f5c34f928076d810074d11e240ff85
ipacheco <ipacheco@despegar.com>
2020-04-08 18:58:13 -0300

    add web server

---
+++ server.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from nytdiff import main
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, News!'
+
+
+@app.route('/check')
+def check_news():
+    main()
